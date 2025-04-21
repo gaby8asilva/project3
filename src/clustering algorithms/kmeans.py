6bb3d32cf457b60bdc325532c3c269cb1ecf341c
danajimenez <danajimenez@ufl.edu>
2025-04-21 12:51:54 -0400

    feat: finalized kmeans clustering and added breakdown

--- src/clustering algorithms/kmeans.py
+++ src/clustering algorithms/kmeans.py
@@ -70,17 +70,29 @@ data_points = features.to_numpy().tolist()
 
 # 3 clusters
 cluster_labels = kmeans(data_points, number_of_clusters=3)
+outfit_data['Cluster'] = cluster_labels
 
 cluster_counts = {}
 for label in cluster_labels:
     cluster_name = f"Cluster {label + 1}"
     cluster_counts[cluster_name] = cluster_counts.get(cluster_name, 0) + 1
-
-
 output = [{"label": cluster, "value": count} for cluster, count in sorted(cluster_counts.items())] # frontend purposes
-
-
 with open("../../frontend/kmeans_results.json", "w") as file:
     json.dump(output, file)
 
 print("K-Means results saved")
+
+# breaking down clusters for visual purposes
+def generate_breakdown(outfit_data, tag_column, output_filename):
+    breakdown = outfit_data.groupby(['Cluster', tag_column]).size().unstack(fill_value=0)
+    output_results = {}
+    for cluster_num, row in breakdown.iterrows():
+        cluster_name = f"Cluster {cluster_num + 1}"
+        output_results[cluster_name] = row[row > 0].to_dict()
+    with open(f"../../frontend/{output_filename}", "w") as fileName:
+        json.dump(output_results, fileName)
+    print(f"Saved {output_filename}")
+
+generate_breakdown(outfit_data, 'Style_Tag', 'style_breakdown_kmeans.json')
+generate_breakdown(outfit_data, 'Color_Tag', 'color_breakdown_kmeans.json')
+generate_breakdown(outfit_data, 'Season_Tag', 'season_breakdown_kmeans.json')
